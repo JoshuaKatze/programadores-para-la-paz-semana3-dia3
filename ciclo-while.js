0f4cb7e9abd97eb1737087dd7c7036398d2f6a95
JoshuaKatze <joshuafernandezestudio@gmail.com>
2026-05-26 10:09:54 -0500

    SoluciÃ³n de la tarea - Automatizado

--- ciclo-while.js
+++ ciclo-while.js
@@ -6,7 +6,7 @@ console.log("Vamos a repetir un proceso mientras el intento sea menor o igual al
 
 while (intento <= maximoIntentos) {
   console.log("Intento de revisión número:", intento)
-  intento = intento + 1
+  intento = intento + 4
 }
 
 console.log("Proceso de revisión finalizado.")
